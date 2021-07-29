38c96cf873ec05973d2f3c0c9a01fc289ae63f11
denamyte <denamyte@gmail.com>
2021-07-30 02:10:42 +0300

    2021-07-30: Stage 1/4: in progress 1

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,1 +1,21 @@
-# Write your code here
+from enum import Enum
+
+
+class State(Enum):
+    MainMenu = 1
+
+
+class BankingSystem:
+
+    def __init__(self):
+        self.state: State = State.MainMenu
+
+    @staticmethod
+    def get_main_menu_input() -> str:
+        return input('''\
+1. Create an account
+2. Log into account
+0. Exit
+''')
+
+
